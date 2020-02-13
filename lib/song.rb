579415f5ecf78ccd1612e47598194cad82db886e
Jesse Griffin <jesse.griffin@gmail.com>
2020-02-13 03:54:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,9 +34,8 @@ class Song
   end 
   
   def self.find_or_create_by_name(name) 
-    if self.find_by_name(name) = nil 
-      return self.create_by_name(name) 
-    else self.find_by_name(name) 
+    find_create = self.find_by_name(name)
+   
     end 
   end 
 
